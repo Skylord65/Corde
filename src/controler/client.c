56e2bad9b1832be57ac007e5bea2ac44cffab370
Manolo-dev <sardo.manolo.dev@gmail.com>
2025-11-11 15:36:17 +0100

    refactor(boundary/client.c) : refonte du caca du patron

--- src/controler/client.c
+++ src/controler/client.c
@@ -18,36 +18,50 @@
 
 
 int main(int argc, char const *argv[]){
+    //---------------------------//
+    // Déclaration des variables //
+    //---------------------------//
+
     Message_t message_recu, message;
+    int sid;
+    struct sockaddr_in dst_serv_addr;
 
-    fill_message(&message, "message");
+    //------------------------------//
+    // Initialisation des variables //
+    //------------------------------//
 
-    if (argc!= 3){
+    dst_serv_addr.sin_family = AF_INET;
+    dst_serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if(argc!= 3) {
         perror("Problème sur le nombre de paramètres.");
         return EXIT_FAILURE;
     }
-    int sid;
+
     sid = socket(AF_INET, SOCK_STREAM, 0);
-    if (sid==-1){
+    
+    if(sid==-1) {
         perror("Erreur dans la création de socket.");
         return EXIT_FAILURE;
     }
-    struct sockaddr_in dst_serv_addr;
-    memset(&dst_serv_addr,0,sizeof(dst_serv_addr));
-    dst_serv_addr.sin_family = AF_INET;
-    dst_serv_addr.sin_port = htons(atoi(argv[2]));
-    inet_pton(AF_INET, argv[1], &dst_serv_addr.sin_addr);
 
-    if (connect(sid, (struct sockaddr*) &dst_serv_addr, sizeof(dst_serv_addr))==-1){
+    fill_message(&message, "message");
+    memset(&dst_serv_addr, 0, sizeof(dst_serv_addr)); // Déclarer la mémoire nécessaire
+    inet_pton(AF_INET, argv[1], &dst_serv_addr.sin_addr); // Attribuer l'ip pour le socket
+
+    if(connect(sid, (struct sockaddr*) &dst_serv_addr, sizeof(dst_serv_addr))==-1) {
         perror("Erreur lors de la connection.");
         close(sid);
         return EXIT_FAILURE;
     }
 
-    pid_t pid=fork();
+    //-----------------------------------//
+    // Séparation du client et du server //
+    //-----------------------------------//
 
-    switch (pid)
-        {
+    pid_t pid = fork();
+
+    switch(pid) {
         case -1:
             perror("Erreur de la création de fils receptioniste des messages.");
             return EXIT_FAILURE;
@@ -59,7 +73,7 @@ int main(int argc, char const *argv[]){
                 char c;
                 int i = 0;
                 scanf("%c", &c);
-                while (c != '\n' && i<LG_MAX-1) {
+                while(c != '\n' && i<LG_MAX-1) {
                     buffer[i] = c;
                     scanf("%c", &c);
                     i++;
@@ -79,9 +93,7 @@ int main(int argc, char const *argv[]){
         default:
             break;
         }
-
     do {
-        
         receive_message(&message_recu, sid);
         if (message_recu.lg_message == 0) {
             printf("Le serveur a fermé la connexion.\n");
@@ -90,7 +102,6 @@ int main(int argc, char const *argv[]){
         }
         printf("(server) :");
         print_message(message_recu);
-        
     } while (1);
     
     close(sid);
